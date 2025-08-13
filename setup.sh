74229da8c913059e952032f387d60d55b9e078bd
hangwu <wuh492465@gmail.com>
2025-08-13 02:33:00 +0000

    modify setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 conda install -c nvidia cuda-toolkit=12.4
 pip install torch==2.6.0
-pip install flash_attn --no-build-isolation
+pip install flash_attn==2.7.3 --no-build-isolation
 pip install -r requirements.txt
